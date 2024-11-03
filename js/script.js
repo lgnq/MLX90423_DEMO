1036d4826f9bb1294ca1140a2964737ae0229dcb
lgnq <dzzxzz@gmail.com>
2024-11-03 16:28:58 +0800

    change "Change" to "change"

--- js/script.js
+++ js/script.js
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   darkMode.addEventListener('click', clickDarkMode);
   myInput.addEventListener('keydown', writeCmd);
   sampleSize.addEventListener('keydown', setSampleSize);
-  // sampleFreq.addEventListener('Change', changeSampleFreq);
+  sampleFreq.addEventListener('change', changeSampleFreq);
 
   size = parseInt(sampleSize.value);
 
